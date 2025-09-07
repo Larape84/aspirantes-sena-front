207b16b9371a28103883de804c52712a40d15af9
Leonardo Arape <leonardo.arape@tiendasisimo.com>
2025-09-07 18:27:02 -0500

    configuracion de modulos

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,68 +3,45 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    }
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example',
+    },
 ];
 
 export const compactNavigation: FuseNavigationItem[] = [
-    // {
-    //     id   : 'Dependencias',
-    //     title: 'Dependencias',
-    //     type : 'basic',
-    //     icon : 'heroicons_outline:circle-stack',
-    //     link : '/app/dependencias',
-
-    // },
     {
-        id   : 'Usuarios',
-        title: 'Código QR',
-        type : 'basic',
-        icon : 'heroicons_outline:users',
-        link : '/codigoQr'
-    },
-
-    {
-        id   : 'ScanCodigo',
-        title: 'Scanear QR',
-        type : 'basic',
-        icon : 'heroicons_outline:camera',
-        link : '/scanQr'
+        id: 'anexarDocumento',
+        title: 'Gestion de convocatorias',
+        type: 'basic',
+        icon: 'heroicons_outline:camera',
+        link: '/scanQr',
     },
     {
-        id   : 'usuarioCreado',
+        id: 'usuarioCreado',
         title: 'Usuarios',
-        type : 'basic',
-        icon : 'heroicons_outline:user-group',
-        link : '/usuarios'
+        type: 'basic',
+        icon: 'heroicons_outline:user-group',
+        link: '/usuarios',
     },
-    {
-        id   : 'registros',
-        title: 'Registros',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/registros'
-    }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    }
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example',
+    },
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    }
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example',
+    },
 ];
